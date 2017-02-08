dc6d7c0abab137e5d4daddd049f43aaa6832f4ad
Mark Walker <markwal@hotmail.com>
2017-02-08 22:52:19 +0000

    Let's just remove the version check.
    It's only been annoying and there are other ways to disable the plugin
    in the case of incompatibility.

--- octoprint_ScreenSquish/__init__.py
+++ octoprint_ScreenSquish/__init__.py
@@ -2,16 +2,10 @@
 from __future__ import absolute_import
 
 import octoprint.plugin
-from pkg_resources import parse_version
 
 class ScreenSquishPlugin(octoprint.plugin.AssetPlugin, octoprint.plugin.TemplatePlugin, octoprint.plugin.SettingsPlugin):
 	def __init__(self):
-		# when octoprint is >= to octoprint_max_version, disable this plugin
-		# automatically because of the fragile internal dependencies on HTML
-		# this is the maintain by hand method, eventually responsive will be built
-		# in or we'll do a complete alternate UI and we won't maintain it this
-		# way
-		self.octoprint_max_version = "1.3.1"
+		pass
 
 	def get_settings_defaults(self):
 		return dict(
@@ -19,34 +13,10 @@ class ScreenSquishPlugin(octoprint.plugin.AssetPlugin, octoprint.plugin.Template
 		)
 
 	def get_assets(self):
-		from octoprint._version import get_versions
-
-		octoprint_version = self._parse_version(get_versions()["version"])
-		settings_max_version = self._parse_version(self._settings.get(["octoprint_max_version"]))
-		default_max_version = self._parse_version(self.octoprint_max_version)
-
-		if octoprint_version < default_max_version:
-			self._settings.set(["octoprint_max_version"], None)
-		elif settings_max_version is not None and settings_max_version != '':
-			if octoprint_version > settings_max_version:
-				return dict(js=["js/squishsettings.js"])
-		else:
-			return dict(js=["js/squishsettings.js"])
-		
 		return dict(
 			js=["js/squish.js"],
 			css=["css/squish.css"]
 		)
-
-	def _parse_version(self, version):
-		if version is None or version == '':
-			return version
-		# if setuptools is old, let's just use the first two tuple elements,
-		# otherwise we'll leave the comparison up to the Version objects
-		version = parse_version(version)
-		if isinstance(version, tuple):
-			return version[:2]
-		return version
 
 __plugin_name__ = "ScreenSquish"
 
